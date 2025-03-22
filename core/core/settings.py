cf8c4f4511ba36b48b281503c81a89d74f11183f
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-03-22 11:40:49 +0500

    Update Settings

--- core/core/settings.py
+++ core/core/settings.py
@@ -93,9 +93,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uz'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
