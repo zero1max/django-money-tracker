1b7274154ffdc6e6f79ad4431dbf7a33b51f3cce
zero1max <nigmatjonovmuhammadjon08@gmail.com>
2025-03-23 22:44:47 +0500

    Update Models

--- core/root/models.py
+++ core/root/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class SavingsGoal(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    goal_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Maqsad summa
-    current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Hozircha yig‘ilgan summa
+    goal_amount = models.DecimalField(max_digits=10, decimal_places=2)  
+    current_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0) 
 
     def is_goal_reached(self):
         return self.current_amount >= self.goal_amount
